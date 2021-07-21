7987c6850837e12d6851fdc41af4914c865ed175
Meliscom89 <Meliscom@aol.com>
2021-07-21 16:29:26 -0400

    pre debug final

--- js/directory.js
+++ js/directory.js
@@ -1,7 +1,31 @@
-let directory = "../json/directory.json";
-
-fetch(directory)
-  .then((response) => response.json())
+fetch('../json/directory.json')
   .then((response) => {
-    console.log(response);   
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+
+    let x = 0
+    let comp_name = document.querySelectorAll('.dir_name');
+    let phone = document.querySelectorAll('.dir_phone');
+    let address = document.querySelectorAll('.dir_address');
+    let img = document.querySelectorAll('.dir_logo');
+    let website = document.querySelectorAll('.dir_website')
+    
+    let list = data.directory;
+
+    for (item of list) {
+  
+          comp_name[x].innerHTML = item.comp_name;
+          phone[x].innerHTML = item.phone;
+          address[x].innerHTML = item.address;
+          img[x].setAttribute('src','images/' + item.logo);
+          img[x].setAttribute('alt','images/' + item.comp_name);
+          website[x].setAttribute('href', item.website);
+          website[x].innerHTML = item.website;
+
+          x++;
+        
+      }
+
     });
--- js/events.js
+++ js/events.js
@@ -1,40 +1,28 @@
-events = '../json/opcc.json';
+// events = 'json/opcc.json';
 
 fetch('../json/opcc.json')
-  .then((response) => 
-    console.log(response)
+  .then((response) => {
+    return response.json();
+  })
+  .then(json => {
+    console.log(json);
 
+    let x = 0
+    let name = document.querySelectorAll('.card .event_name');
+    let date = document.querySelectorAll('.card .event_date');
+    let time = document.querySelectorAll('.card .event_time');
+    let location = document.querySelectorAll('.card .event_location');
+    let list = json.opcc[0].events;
 
-
-
-
-
-
-
-
-);
-// const data = "../data/opcc.json";
-// fetch(data)
-//   .then((response) => response.json())
-//   .then((response) => {
-//     console.log(response);
-
-//     let x = 0
-//     let name = document.querySelectorAll('.card .event_name');
-//     let date = document.querySelectorAll('.card .event_date');
-//     let time = document.querySelectorAll('.card .event_time');
-//     let location = document.querySelectorAll('.card .event_location');
-//     let list = response.opcc[0].events;
-
-//     for (item of list) {
+    for (item of list) {
   
-//           name[x].innerHTML = item.eventName;
-//           date[x].innerHTML = item.date;
-//           time[x].innerHTML = item.time;
-//           location[x].innerHTML = item.location;
-//           x++;
+          name[x].innerHTML = item.eventName;
+          date[x].innerHTML = item.date;
+          time[x].innerHTML = item.time;
+          location[x].innerHTML = item.location;
+          x++;
         
-//       }
+      }
 
-//     });
+    });
    
